0eba55704d33aea6e6e3362d3848b76b6130d351
Chris Molozian <chris@cmoz.me>
2012-03-12 21:20:23 +0000

    Small amount of code cleanup, added software LICENSE and bumped the version for the first release.

--- src/main/java/com/pieceof8/gradle/snapshot/GitcommitTask.java
+++ src/main/java/com/pieceof8/gradle/snapshot/GitcommitTask.java
@@ -1,4 +1,19 @@
 // vi: set softtabstop=4 shiftwidth=4 expandtab:
+/*
+ * Copyright 2012 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.pieceof8.gradle.snapshot;
 
 import static java.lang.String.format;
@@ -76,7 +91,7 @@ class GitcommitTask extends DefaultTask {
 
         extension = project.getExtensions()
                 .getByType(SnapshotPluginExtension.class);
-        output = new File(outputDir, extension.getSnapshotPropertiesFilename());
+        output = new File(outputDir, extension.getFilename());
         log = project.getLogger();
     }
 
--- src/main/java/com/pieceof8/gradle/snapshot/SnapshotPluginExtension.java
+++ src/main/java/com/pieceof8/gradle/snapshot/SnapshotPluginExtension.java
@@ -1,4 +1,19 @@
 // vi: set softtabstop=4 shiftwidth=4 expandtab:
+/*
+ * Copyright 2012 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.pieceof8.gradle.snapshot;
 
 import org.gradle.api.Project;
@@ -19,7 +34,7 @@ class SnapshotPluginExtension {
      * defaults to {@code snapshot.properties}.
      */
     @Getter @Setter
-    private @NonNull String snapshotPropertiesFilename;
+    private @NonNull String filename;
 
     /**
      * The date format to be used for any dates exposed in properties. This
@@ -43,7 +58,7 @@ class SnapshotPluginExtension {
      * @param project The project this extension is being applied to.
      */
     public SnapshotPluginExtension(final @NonNull Project project) {
-        setSnapshotPropertiesFilename("snapshot.properties");
+        setFilename("snapshot.properties");
         setDateFormat("dd.MM.yyyy '@' HH:mm:ss z");
         setVerbose(false);
     }
